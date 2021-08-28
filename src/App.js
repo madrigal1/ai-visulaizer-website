7d38a1de5237219454c6651f40399dc2287f932f
madrigal1 <kurupgokul11@gmail.com>
2021-08-28 20:30:00 +0530

    feat: setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import ForceGraph from './components/ForceGraph';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="graph">
+        <ForceGraph />
+      </div>
+      <div className="settings">
+
+      </div>
     </div>
   );
 }
